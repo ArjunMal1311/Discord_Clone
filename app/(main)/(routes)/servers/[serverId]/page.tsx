126a1745c8835b7d4b3204a7c7d70296d74c926a
Arjun Malhotra <hiarjun.malhotra2002@gmail.com>
2023-09-03 15:29:49 +0530

    Navigation bar completed

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,10 @@
+
+const ServerIdPage = () => {
+    return (
+        <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3">
+            Hello
+        </div>
+    )
+}
+
+export default ServerIdPage
