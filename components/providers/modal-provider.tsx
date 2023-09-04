a622434cc49f7a562bb9fa73ddc95c40ebeafd92
Arjun Malhotra <hiarjun.malhotra2002@gmail.com>
2023-09-04 13:16:37 +0530

    Updated Routes and added delete, create channel

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,9 @@ import { CreateServerModal } from "../modals/create-server-model";
 import { InviteModal } from "../modals/invite-modal";
 import { EditServerModal } from "../modals/edit-server-model";
 import { MembersModal } from "../modals/members-modal";
+import { CreateChannelModal } from "../modals/create-channel-model";
+import { LeaveServerModal } from "../modals/leave-server-model";
+import { DeleteServerModal } from "../modals/delete-server-model";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -23,6 +26,9 @@ export const ModalProvider = () => {
             <InviteModal />
             <EditServerModal />
             <MembersModal />
+            <CreateChannelModal />
+            <LeaveServerModal />
+            <DeleteServerModal />
         </>
     )
 }
